45b3c142116d352a887de21136a8b94959f7f882
ethandrzb <ethandrzb@gmail.com>
2021-10-11 20:49:55 -0400

    Start of Solution41

---
+++ exercise41/src/main/java/baseline/Solution41.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution41
+{
+    public static void main(String[] args)
+    {
+        // Read input file and store in sortable data structure
+
+        // Sort data structure by last name
+
+        // Generate output
+
+        // Write output to file
+
+        // Exit
+    }
+}
