1a8e36f0b235e50cc29e800b371e9d2fc87dbf3f
ethandrzb <ethandrzb@gmail.com>
2021-10-16 19:52:38 -0400

    Start of Solution44

---
+++ exercise44/src/main/java/baseline/Solution44.java
@@ -0,0 +1,48 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution44
+{
+    public static void main(String[] args)
+    {
+        // Read product database
+
+        // Begin search for product
+
+        // Exit
+    }
+
+    public void searchForProduct(String query)
+    {
+        // <infinite loop>
+            // Check if query matches any name in database
+                // If so, break from loop
+                // If not, v error message
+
+        // Get product info
+    }
+
+    private void readJSONFile()
+    {
+        // Attempt to read JSON file
+
+        // If successful, convert to map of objects
+        // If not, print error message
+    }
+}
+
+class Product
+{
+    private String name;
+    private double price;
+    private int quantity;
+
+    public String toString()
+    {
+
+    }
+}
