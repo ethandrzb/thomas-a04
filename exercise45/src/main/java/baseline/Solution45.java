43f2d752e49167932b5c9efd58c29230e049f4b7
ethandrzb <ethandrzb@gmail.com>
2021-10-17 15:14:25 -0400

    Start of Solution45

---
+++ exercise45/src/main/java/baseline/Solution45.java
@@ -0,0 +1,31 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solutions
+ *  Copyright 2021 Ethan Thomas
+ */
+
+package baseline;
+
+public class Solution45
+{
+    public static void main(String[] args)
+    {
+        // Prompt user for output filename
+
+        // Find and replace all occurrences of "utilize" with "use"
+
+        // Exit
+    }
+
+    public void findAndReplace(String inputFileName, String outputFileName)
+    {
+        // Attempt to read input text file
+            // Attempt to open/create output text file
+                // while there are still lines of text to read from the input file
+
+                    // Read single line
+
+                    // Replace all occurrences of the word "utilize" with use
+
+                    // Write to output file
+    }
+}
